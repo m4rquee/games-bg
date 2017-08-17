2676da1d6dac2885d3b0831438b63b29f672368b
LucasXM12 <lucasxm21@hotmail.com>
2017-08-17 10:54:54 -0300

    Format

--- Pong/Assets/Scripts/RacketMov.cs
+++ Pong/Assets/Scripts/RacketMov.cs
@@ -5,18 +5,18 @@ public class RacketMov {
 
     private float maxSpeed;
 
-    private Rigidbody2D [] rb2d;
+    private Rigidbody2D[] rb2d;
 
     public RacketMov(Rigidbody2D rb2dP1, Rigidbody2D rb2dP2, float maxSpeed) {
-        this.rb2d = new Rigidbody2D [2];
+        this.rb2d = new Rigidbody2D[2];
 
-        this.rb2d [0] = rb2dP1;
-        this.rb2d [1] = rb2dP2;
+        this.rb2d[0] = rb2dP1;
+        this.rb2d[1] = rb2dP2;
 
         this.maxSpeed = maxSpeed;
     }
 
     public void move(int player, List<object> vel) {
-        this.rb2d [player].velocity = new Vector2(0, (float) vel [0] * maxSpeed);
+        this.rb2d[player].velocity = new Vector2(0, (float) vel[0] * maxSpeed);
     }
 }
