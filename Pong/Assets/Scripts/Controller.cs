d4297def0f433c32656ad4784a441783f94fc568
Lucas de Oliveira Silva <lucasxm21@hotmail.com>
2017-09-17 18:39:10 -0300

    IRobot update

--- Pong/Assets/Scripts/Controller.cs
+++ Pong/Assets/Scripts/Controller.cs
@@ -4,51 +4,51 @@ using UnityEngine;
 using System.Collections.Generic;
 
 public class Controller: MonoBehaviour {
-    public GameObject ball;
+	public GameObject ball;
 
-    public GameObject player1;
-    public GameObject player2;
+	public GameObject player1;
+	public GameObject player2;
 
-    public float maxSpeed;
+	public float maxSpeed;
 
-    private RacketMov racketMov;
-    private GameManager gameManager;
+	private RacketMov racketMov;
+	private GameManager gameManager;
 
-    private Dictionary<string, object>[] states;
+	private Dictionary<string, object>[] states;
 
-    void Start() {
-        Time.timeScale = 3;
-        this.racketMov = new RacketMov(player1.GetComponent<Rigidbody2D>(),
-            player2.GetComponent<Rigidbody2D>(), this.maxSpeed);
+	void Start() {
+		Time.timeScale = 3;
+		this.racketMov = new RacketMov(player1.GetComponent<Rigidbody2D>(),
+			player2.GetComponent<Rigidbody2D>(), this.maxSpeed);
 
-        var actions = new Dictionary<string, Action<int, List<object>>>
-        {{ "move", this.racketMov.move }};
+		var actions = new Dictionary<string, Action<int, List<object>>>
+		{{ "move", this.racketMov.move }};
 
-        this.gameManager = new GameManager(new string[]
-            { "D:\\PD\\TCC\\MyRobot.dll", "D:\\PD\\TCC\\MyRobot.dll" }, actions);
+		this.gameManager = new GameManager(new string[]
+			{ "D:\\PD\\TCC\\MyRobot.dll", "D:\\PD\\TCC\\MyRobot.dll" }, actions, 2);
 
-        this.states = new Dictionary<string, object>[2];
-    }
+		this.states = new Dictionary<string, object>[2];
+	}
 
-    void Update() {
-        this.states[0] = new Dictionary<string, object>();
-        this.states[1] = new Dictionary<string, object>();
+	void Update() {
+		this.states[0] = new Dictionary<string, object>();
+		this.states[1] = new Dictionary<string, object>();
 
-        float ballX = this.ball.transform.position.x,
-        ballY = this.ball.transform.position.y;
+		float ballX = this.ball.transform.position.x,
+		ballY = this.ball.transform.position.y;
 
-        float plyrX = this.player1.transform.position.x,
-        plyrY = this.player1.transform.position.y;
+		float plyrX = this.player1.transform.position.x,
+		plyrY = this.player1.transform.position.y;
 
-        this.states[0].Add("ball-pos", new float[] { ballX, ballY });
-        this.states[0].Add("player-pos", new float[] { plyrX, plyrY });
+		this.states[0].Add("ball-pos", new float[] { ballX, ballY });
+		this.states[0].Add("player-pos", new float[] { plyrX, plyrY });
 
-        plyrX = this.player2.transform.position.x;
-        plyrY = this.player2.transform.position.y;
+		plyrX = this.player2.transform.position.x;
+		plyrY = this.player2.transform.position.y;
 
-        this.states[1].Add("ball-pos", new float[] { ballX, ballY });
-        this.states[1].Add("player-pos", new float[] { plyrX, plyrY });
+		this.states[1].Add("ball-pos", new float[] { ballX, ballY });
+		this.states[1].Add("player-pos", new float[] { plyrX, plyrY });
 
-        gameManager.update(this.states);
-    }
+		gameManager.Update(this.states);
+	}
 }
--- Pong/Assets/Scripts/RandSpeed.cs
+++ Pong/Assets/Scripts/RandSpeed.cs
@@ -2,46 +2,46 @@
 
 public class RandSpeed: MonoBehaviour {
 
-    public float speed;
-    public float randomRange;
+	public float speed;
+	public float randomRange;
 
-    private Rigidbody2D rb2d;
+	private Rigidbody2D rb2d;
 
-    private int[] score = new int[] { 0, 0 };
+	private int[] score = new int[] { 0, 0 };
 
-    void Start() {
-        this.rb2d = GetComponent<Rigidbody2D>();
+	void Start() {
+		this.rb2d = GetComponent<Rigidbody2D>();
 
-        this.rb2d.velocity = (Random.value > 0.5 ? Vector2.right : Vector2.left) * speed;
-    }
+		this.rb2d.velocity = (Random.value > 0.5 ? Vector2.right : Vector2.left) * speed;
+	}
 
-    void OnCollisionEnter2D(Collision2D collision) {
-        switch (collision.gameObject.tag) {
-            case "racket":
-            float dirY = (transform.position.y - collision.transform.position.y)
-            / collision.collider.bounds.size.y, dirX = 0;
+	void OnCollisionEnter2D(Collision2D collision) {
+		switch (collision.gameObject.tag) {
+			case "racket":
+				float dirY = (transform.position.y - collision.transform.position.y)
+				/ collision.collider.bounds.size.y, dirX = 0;
 
-            if (collision.gameObject.name == "racketl")
-                dirX = 1;
-            else if (collision.gameObject.name == "racketr")
-                dirX = -1;
+				if (collision.gameObject.name == "racketl")
+					dirX = 1;
+				else if (collision.gameObject.name == "racketr")
+					dirX = -1;
 
-            Vector2 dir = new Vector2(dirX, dirY + Random.Range(-randomRange, randomRange)).normalized;
+				Vector2 dir = new Vector2(dirX, dirY + Random.Range(-randomRange, randomRange)).normalized;
 
-            this.rb2d.velocity = dir * this.speed;
-            break;
+				this.rb2d.velocity = dir * this.speed;
+				break;
 
-            case "wall":
-            if (collision.gameObject.name == "walll")
-                this.score[0]++;
-            else if (collision.gameObject.name == "wallr")
-                this.score[1]++;
+			case "wall":
+				if (collision.gameObject.name == "walll")
+					this.score[0]++;
+				else if (collision.gameObject.name == "wallr")
+					this.score[1]++;
 
-            Debug.Log("Score: " + this.score[0] + " " + this.score[1]);
+				Debug.Log("Score: " + this.score[0] + " " + this.score[1]);
 
-            this.transform.position = Vector3.zero;
-            this.rb2d.velocity = (Random.value > 0.5 ? Vector2.right : Vector2.left) * speed;
-            break;
-        }
-    }
+				this.transform.position = Vector3.zero;
+				this.rb2d.velocity = (Random.value > 0.5 ? Vector2.right : Vector2.left) * speed;
+				break;
+		}
+	}
 }
