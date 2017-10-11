69339ca9c7e376284791e499feb50675cec9fab4
LucasXM12 <lucasxm21@hotmail.com>
2017-10-11 11:18:05 -0300

    Finished movements

--- RobAICode/Assets/_Completed-Assets/Scripts/Managers/GameCtrl.cs
+++ RobAICode/Assets/_Completed-Assets/Scripts/Managers/GameCtrl.cs
@@ -7,32 +7,71 @@ using System.Collections.Generic;
 public class GameCtrl: MonoBehaviour {
 
 	private GameManager gameManager;
+	private GameObject[] tanksObjs;
 	private Dictionary<string, object>[] states;
-	private Complete.TankMovement[] tanks = new Complete.TankMovement[2];
+	private Dictionary<string, Action<int, float>> tankActions;
 
-	// Use this for initialization
 	void Start() {
-		var tanksObjs = GameObject.FindGameObjectsWithTag("Tank");
-		this.tanks[0] = tanksObjs[0].GetComponent<Complete.TankMovement>();
-		this.tanks[1] = tanksObjs[1].GetComponent<Complete.TankMovement>();
+		this.tankActions = new Dictionary<string, Action<int, float>>()
+		{{ "move", SetMovementInputValue}, { "turn", SetTurnInputValue}};
+
+		this.tanksObjs = GameObject.FindGameObjectsWithTag("Tank");
+
+		var actions = new Dictionary<string, Action<int, List<object>>>
+			{{ "DoAction", DoAction }};
+
+		this.states = new Dictionary<string, object>[2];
+		this.states[0] = new Dictionary<string, object>();
+		this.states[1] = new Dictionary<string, object>();
 
 		try {
-			var actions = new Dictionary<string, Action<int, List<object>>>
-			{{ "movement", Movement }};
-
 			this.gameManager = new GameManager(new string[] { "", "" }, actions, 2);
-
-			this.states = new Dictionary<string, object>[2];
-			this.states[0] = new Dictionary<string, object>();
-			this.states[1] = new Dictionary<string, object>();
 		} catch (Exception e) {
 			Debug.Log(e.StackTrace);
 		}
 	}
 
 	void Update() {
+		double p1X = this.tanksObjs[0].transform.position.x,
+			p1Y = this.tanksObjs[0].transform.position.y;
 
+		double p2X = this.tanksObjs[1].transform.position.x,
+		p2Y = this.tanksObjs[1].transform.position.y;
+
+		this.states[0]["enemy-pos"] = new ArrayList { p2X, p2Y };
+		this.states[0]["player-pos"] = new ArrayList { p1X, p1Y };
+
+		this.states[1]["enemy-pos"] = new ArrayList { p1X, p1Y };
+		this.states[1]["player-pos"] = new ArrayList { p2X, p2Y };
+
+		gameManager.Update(this.states);
 	}
 
-	public void Movement(int plyrs, List<object> p) { }
+	//p[0] = "action1-action2-action3- ... -actionn", p[1] = {pA1, pA2, pA3 ... pAn}
+	public void DoAction(int plyr, List<object> p) {
+		string[] actions = ((string) p[0]).Split('-');
+
+		for (int i = 0; i < actions.Length; i++)
+			this.tankActions[actions[i]](plyr, (float) (double) p[i]);
+	}
+
+	private void SetMovementInputValue(int plyr, float v) {
+		v /= 100;
+		if (v > 1)
+			v = 1;
+		else if (v < -1)
+			v = -1;
+
+		this.tanksObjs[plyr].GetComponent<Complete.TankMovement>().M_MovementInputValue = v;
+	}
+
+	private void SetTurnInputValue(int plyr, float v) {
+		v /= 100;
+		if (v > 1)
+			v = 1;
+		else if (v < -1)
+			v = -1;
+
+		this.tanksObjs[plyr].GetComponent<Complete.TankMovement>().M_TurnInputValue = v;
+	}
 }
